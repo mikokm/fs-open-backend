9abf4c1e4d6d1e57d573bc443a6ac45e704e3c72
Miko Mynttinen <mikokm@gmail.com>
2021-02-14 22:22:41 +0100

    3.7 - 3.8

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const { response } = require('express')
 const express = require('express')
+var morgan = require('morgan')
+
 const app = express()
 app.use(express.json())
+
+let logger = morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    req.method === 'POST' ? JSON.stringify(req.body) : ''
+  ].join(' ')
+})
+
+app.use(logger)
 
 let persons = [
   {
