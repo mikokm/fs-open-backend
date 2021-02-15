46c9131625af6fb31dcfd64da4d1c9abee438408
Miko Mynttinen <mikokm@gmail.com>
2021-02-15 19:33:29 +0100

    uibuild

--- index.js
+++ index.js
@@ -106,7 +106,6 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    console.log("validation")
     return response.status(400).json({ error: error.message })
   }
 
