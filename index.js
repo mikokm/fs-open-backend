721d0ebe1dbfef7bf9d687822a0a2f5a1c48c47b
Miko Mynttinen <mikokm@gmail.com>
2021-02-15 14:40:17 +0100

    Port and Procfile

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { response } = require('express')
 const express = require('express')
-var morgan = require('morgan')
+const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 let logger = morgan(function (tokens, req, res) {
@@ -91,7 +94,7 @@ app.get('/info', (req, res) => {
   res.send(message)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
